d9f4753f456daa4c8257bb0d35274f697aabb1c8
ceco <combytec@gmail.com>
2015-08-14 12:18:10 +0300

    Phonebook with complete add, update and delete contact. Sort and search possibility.

---
+++ PHP project/Phonebook/footer.php
@@ -0,0 +1,7 @@
+			<footer>
+				Copyright 2014 - <?php echo date("Y"); ?>
+			</footer>
+		</div>
+	</div>	
+</body>
+</html>
---
+++ PHP project/Phonebook/nav.php
@@ -0,0 +1,28 @@
+<header>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class = "container-fluid">
+		<div class = "navbar-header">
+			<a class = "navbar-brand" href="index.php">Тел.Указател</a>
+		</div>
+			<ul  class="nav navbar-nav">
+				<li class = "active">
+					<a href="index.php">
+						Контакти
+					</a>
+				</li><li>
+					<a href="newcontact.php">
+						Нов Контакт
+					</a>
+				</li><li>
+					<a href="edit.php">
+						Редактиране
+					</a>
+				</li><li>
+					<a href="remove.php">
+						Изтриване
+					</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</header>
