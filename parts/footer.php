05b197a476cf5814304aebefd83223348951e747
georgiev_h_tsvetan@yahoo.com <Smorkov_01>
2016-03-29 13:49:35 +0300

    radieli footer for every page

---
+++ parts/footer.php
@@ -0,0 +1,33 @@
+		<footer>
+			<div id="footerg">
+				<div class="container">
+					<div class="three">
+						<h3>Връзка с хотел Радиели</h3>
+						<p>с. Дряново</p>
+						<p>тел. <a href="tel:+359887740904">0887 74 09 04</a></p>
+						<p>тел. <a href="tel:+359885150439">0885 15 04 39</a></p>
+						<p>тел. <a href="tel:+359896993363">0896 99 33 63</a></p>
+						<p class="small">При щракане на някой от номерата, той се зарежда за набиране</p>
+					</div>
+					<div class="three">
+						<h3>Електронна поща / e-mail</h3>
+						<p>radieli_sf@abv.bg</p>
+					</div>
+					<div class="three">
+						<h3>Фейсбук група</h3>
+						<p>
+							<a href="https://www.facebook.com/HotelRadieli/" target="_blank">Facebook:  Хотел Радиели/Hotel Radieli</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div id="footerb">
+				<div class="container">
+					<h3>Информационен сайт</h3>
+					<h3>Хотел Радиели</h3>
+				</div>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
