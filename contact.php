ea9f27689949b1d3b14209c473aa55f430a84e55
georgiev_h_tsvetan@yahoo.com <Smorkov_01>
2016-04-14 22:06:44 +0300

    Working on email form. Time to understand PHPMailer

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 	include_once('parts/header.php');
 ?>
-	<meta name="description" content="Карта на офиса на Иван Тричков. Както и контактна форма за директно изпращане на емейл (електронно писмо) до електронната поща на Иван Тричков">
+	<meta name="description" content="Карта на хотел Радиели с. Дряново. Както и контактна форма за директно изпращане на емейл (електронно писмо) до електронната поща на хотел Радиели">
 	<title>Контакти | хотел Радиели до кръстова гора | hotelradieli.com </title>
 </head>
 <body>
@@ -97,29 +97,96 @@
 		</header>
 		<div id="content">
 			<article id="contactarea">
-				<h2 style="display:none">Begin google map with the address of the office</h2>
+				<h2 style="display:none">Begin google map with the address of the area</h2>
+				<?php
+				//				if(!$mail->send()) {
+				//					echo '<p>Съобщението не е доставено.</p>';
+				//					echo '<p>Грешката е: ' . $mail->ErrorInfo.'</p>';
+				//				} else {
+				//					echo '<p>Съобщението е успешно доставено!</p>';
+				//				}
+				function validation(){
+					if(isset($_POST["sendemail"])){
+						$podatel = $_POST['name'];
+						$email_sender = $_POST['email'];
+						$otnosno = $_POST['subject'];
+						$tqlo = $_POST['message'];
+						if ($podatel && $email_sender && $tqlo){
+							include_once 'scripts/sendMail.php';
+							if (!$mail->send()){
+								echo '<p>Съобщението не е доставено.</p>';
+								echo '<p>Грешката е: '.$mail->ErrorInfo.'</p>';
+							}
+							else{
+								echo '<p>Съобщението е успешно доставено!</p>';
+							}
+							return false;
+						}
+						else{
+							return true;
+						}
+					}
+					return false;
+				}
+				if (validation()){
+					echo '<h3 style = "color: #660000"> Някое от полетата за изпращане на електронно писмо не са въведени.</h3>';
+				}
+				?>
 				<p>
-					За да ме откриете, може да видите картата или направо по-долу да бъдете навигирани от мястото, на което се намирате.
-				</p>
-				<p>
-					Решили сте да ми пишете. Моля, възползвайте се от бутона, който е залепен за лявата граница на прозорецът.
+					За да ни откриете, може да видите картата или направо по-долу да бъдете навигирани от мястото, на което се намирате.
 				</p>
 				<p class="clear"></p>
 				<div class="google_map">
 					<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=%D0%9F%D0%BB%D0%BE%D0%B2%D0%B4%D0%B8%D0%B2%20%D1%83%D0%BB%D0%B8%D1%86%D0%B0%20%D0%A5%D0%B0%D0%BD%20%D0%90%D1%81%D0%BF%D0%B0%D1%80%D1%83%D1%85%20105&key=AIzaSyCq3ELAWsse2U8le7E9Q4hw9vaAavm2skc"></iframe>
 				</div>
 			</article>
-			<article>
-				<h2 style="display:none">Navigation to the office</h2>
+			<article class="upspacenew">
+				<h2>Навигация</h2>
 				<form action="http://maps.google.com/maps" method="get" target="_blank">
 				   <label for="saddr">
 				   		Напишете мястото, от което искате да дойдете</label>
-				   <input type="text" name="saddr" />
+				   <input type="text" name="saddr" placeholder="Точка А" />
 				   <input type="hidden" name="daddr" value="хотел Радиели с.Дряново" />
 				   <input type="submit" value="Навигирай ме" />
 				</form>
 			</article>
-			<article>
+			<article class="upspacenew">
+				<h2>
+					Можете да ни изпратите e-mail съобщение посредством формата за контакт по-долу:
+				</h2>
+				<p>
+					Полетата отбелязани със <span class="red">*</span> са задължителни
+				</p>
+				<!-- Contact form -->
+				<form name="sendemail" action="<?php $_SERVER['PHP_SELF'];?>" method="POST">
+					<label for="contact-ime">
+						Подател<span class="red">*</span>:
+					</label>
+					<br>
+						<input id="contact-ime" name="name" placeholder="Вашето име" style="cursor: auto;">
+					<br>
+					<label for="contact-email">
+						Вашият емейл <span class="red">*</span>:
+					</label>
+					<br>
+						<input type="email" id="contact-email" name="email" placeholder="Вашият email адрес">
+					<br>
+					<label for="contact-subject">
+						Относно:
+					</label>
+					<br>
+						<input id="contact-subject" name="subject" placeholder="Тема">
+					<br>
+					<label for="contact-message">
+						Текст <span class="red">*</span>:
+					</label>
+					<br>
+						<textarea id="contact-message" name="message" placeholder="съобщение.."></textarea>
+					<br>
+					<input type="submit" name = "sendemail" value="Изпрати" />
+				</form>
+			</article>
+			<article class="upspacenew">
 				<h4>
 					Споделяне в социалните мрежи
 				</h4>
@@ -137,14 +204,6 @@
 		  $cover.remove();
 		});
 	</script>
-	<script type="text/javascript">
-/* var bcf_settings = { buttonText:'Contact Us', buttonTop:'30%', language:'en_US' }; // Better Contact Form Settings */
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0]; js = d.createElement(s); js.id = id;
-    js.src = "http://bettercontactform.com/contact/media/e/f/ef8dc8e473503a897c8c744c8d5f1ee021950242.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    }(document, "script", "bcf-render"));</script>
-<a id="bcf_trigger" href="http://bettercontactform.com" rel="bcf_trigger"></a>
 <?php
 	include_once('parts/footer.php');
 ?>
