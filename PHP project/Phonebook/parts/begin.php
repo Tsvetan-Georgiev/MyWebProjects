bca90db408af726ea05cdd34e0f662e8650150bc
Tsvetan-Georgiev <georgiev_h_tsvetan@yahoo.com>
2016-12-07 11:11:47 +0200

    Header of every page

---
+++ PHP project/Phonebook/parts/begin.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html lang = "bg">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;  charset=UTF-8"/>
+    <title>
+        Телефонен указател
+    </title>
+    <meta name = "viewport" content="width = device-width, initial-scale = 1">
+    <link rel="stylesheet" type="text/css" href="styles/homepage.css">
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap/css/bootstrap.css">
+    <script type="text/javascript" src="scripts/jquery.min.js"></script>
+    <script type="text/javascript" src="styles/bootstrap/js/bootstrap.js"></script>
+    <script src="scripts/validateForm.js"></script>
+</head>
