259c5022e832f0b2e446702247aa4ad1ef1c512f
Tsvetan-Georgiev <georgiev_h_tsvetan@yahoo.com>
2016-12-09 10:29:10 +0200

    Change menu for small screens

--- PHP project/Phonebook/nav.php
+++ PHP project/Phonebook/nav.php
@@ -1,41 +1,48 @@
 <header>
 	<nav class="navbar navbar-inverse navbar-fixed-top">
 		<div class = "container-fluid">
-		<div class = "navbar-header">
-			<a class = "navbar-brand" href="index.php">Тел.Указател</a>
-		</div>
-			<ul  class="nav navbar-nav">
-				<li class = "active">
-					<a href="index.php">
-						Контакти
-					</a>
-				</li><li>
-					<a href="newcontact.php">
-						Нов Контакт
-					</a>
-				</li><li>
-					<a href="edit.php">
-						Редактиране
-					</a>
-				</li><li>
-					<a href="remove.php">
-						Изтриване
-					</a>
-				</li>
-				<?php
-				include_once ('scripts/connect.php');
-					if(loggedin()){
+  		<div class = "navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class = "navbar-brand" href="index.php">Тел.Указател</a>
+  		</div>
+      <div class="collapse navbar-collapse" id="menu">
+  			<ul  class="nav navbar-nav">
+  				<li class = "active">
+  					<a href="index.php">
+  						Контакти
+  					</a>
+  				</li><li>
+  					<a href="newcontact.php">
+  						Нов Контакт
+  					</a>
+  				</li><li>
+  					<a href="edit.php">
+  						Редактиране
+  					</a>
+  				</li><li>
+  					<a href="remove.php">
+  						Изтриване
+  					</a>
+  				</li>
+  				<?php
+  				include_once ('scripts/connect.php');
+  					if(loggedin()){
 
-				?>
-				<li>
-					<a href="scripts/logout.php">
-						Излизане
-					</a>
-				</li>
-				<?php
-					}
-				?>
-			</ul>
+  				?>
+  				<li>
+  					<a href="scripts/logout.php">
+  						Излизане
+  					</a>
+  				</li>
+  				<?php
+  					}
+  				?>
+  			</ul>
+      </div>
 		</div>
 	</nav>
 </header>
